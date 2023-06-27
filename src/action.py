d3570c4e70d5d7ffe7f884b6866cb67619886427
Alex <alex@spaero.bio>
2023-06-27 15:19:07 -0700

    Fix

--- src/action.py
+++ src/action.py
@@ -4,4 +4,4 @@ import numpy as np
 
 
 def mul(arr1: List[float], arr2: List[float]) -> List[float]:
-    return list(np.array(arr1) * np.array(arr2) * 2)
+    return list(np.array(arr1) * np.array(arr2) * 1)
